ee491bd1c5a63b1b4cdace158bdc84ece8e05556
CaioAzvdo <caioazevedodourado8@gmail.com>
2024-06-19 15:58:21 -0300

    Adicionado método Create no controller de Courses

--- crud-spring/src/main/java/com/projeto/crud_spring/controller/CoursesController.java
+++ crud-spring/src/main/java/com/projeto/crud_spring/controller/CoursesController.java
@@ -1,15 +1,9 @@
 package com.projeto.crud_spring.controller;
-
 import com.projeto.crud_spring.model.Course;
 import com.projeto.crud_spring.repository.CourseRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(origins = "http://localhost:4200")
@@ -25,15 +19,37 @@ public class CoursesController {
     public List<Course> list(){
         return courseRepository.findAll();
     }
-    @Bean
-    CommandLineRunner initDatabase(){
-        return args -> {
-            courseRepository.deleteAll();
-            Course C = new Course();
-            C.setName("Java");
-            C.setCategoria("Back-end");
 
-            courseRepository.save(C);
-        };
+    @PostMapping
+    public ResponseEntity create(@RequestBody Course course){
+        return ResponseEntity.status(201).body(courseRepository.save(course));
+//        courseRepository.save(course);
+//        return ResponseEntity.status(201).body().build();
     }
-}
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //    @Bean
+//    CommandLineRunner initDatabase(){
+//        return args -> {
+//            courseRepository.deleteAll();
+//            Course C = new Course();
+//            C.setName("Java");
+//            C.setCategoria("Back-end");
+//
+//            courseRepository.save(C);
+//        };
+//    }
+
+
